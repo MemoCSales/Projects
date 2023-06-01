e3e14e98f2c4fa775b1af51b113a0fa62f755435
MemoCSales <memocsales@gmail.com>
2023-06-01 17:29:09 -0600

    Function that reproduces the ATOI function

---
+++ ATOI/ft_atoi.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
+
+int	main(void)
+{
+	printf("%d", ft_atoi("     ---+--+1234ab567"));
+	printf("\n");
+	printf("%d", ft_atoi("        +---42 000002"));
+	printf("\n");
+	return (0);
+}
